f76e7d3170fe0c46eccbe5e74265979cab61713a
Anton Sizikov <sizikov.anton@gmail.com>
2015-10-24 10:20:36 +0200

    WebHook payload models added, Octokit referenced

--- src/GitHub.Status/StatusModule.cs
+++ src/GitHub.Status/StatusModule.cs
@@ -1,6 +1,8 @@
 using System;
+using GitHub.Status.Model;
 using Microsoft.Framework.Configuration;
 using Nancy;
+using Nancy.ModelBinding;
 
 namespace GitHub.Status
 {
@@ -23,10 +25,16 @@ namespace GitHub.Status
             Post["/github/status/{key}"] = parameters =>
             {
                 var key = parameters.key.ToString();
-
+                
                 if (string.IsNullOrEmpty(key) || key != Secret)
                 {
                     return HttpStatusCode.Forbidden;
+                }
+                var payload = this.Bind<Payload>();
+                
+                return payload.action;
+                if(payload.issue.pull_request != null){
+                    return payload.issue.body;
                 }
                 return key;
             };
