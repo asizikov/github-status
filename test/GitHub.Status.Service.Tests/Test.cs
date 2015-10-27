c484512e09eb2e162e171d3abe0f1ec6d6b0cf5d
Anton Sizikov <sizikov.anton@gmail.com>
2015-10-27 11:50:11 +0100

    project references fixed

--- test/GitHub.Status.Service.Tests/Test.cs
+++ test/GitHub.Status.Service.Tests/Test.cs
@@ -1,16 +1,17 @@
+using System;
 using Xunit;
-using GitHub.Status.Service;
 
 namespace GitHub.Status.Service
 {
-    public class StatusSerivceTests
+    public class StatusServiceTests
     {
-        public class Constructor {
-        [Fact]
-        public void ThrowsWhenNoConfigurationProvided()
+        public class Constructor
         {
-            
-            Assert.Throws<ArgumentNullException>(() => new StatusService(null));
-        }    
+            [Fact]
+            public void ThrowsWhenNoConfigurationProvided()
+            {
+                Assert.Throws<ArgumentNullException>(() => new StatusSerivce(null));
+            }
+        }
     }
 }
